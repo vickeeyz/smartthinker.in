38fb9834df147e1fddbc255ad2663298cd57ae03
Vikrant Kamble <k.vikrantt@gmail.com>
2015-09-21 00:25:07 -0700

    small fixes

--- public/js/creative.js
+++ public/js/creative.js
@@ -48,10 +48,18 @@
 })(jQuery); // End of use strict
 
 
-// makes sure the whole site is loaded
-jQuery(window).load(function () {
-    // will first fade out the loading animation
-    jQuery("#status").fadeOut();
-    // will fade out the whole DIV that covers the website.
-    jQuery("#preloader").delay(1000).fadeOut("slow");
-})
+//// makes sure the whole site is loaded
+//jQuery(window).load(function () {
+//    // will first fade out the loading animation
+//    jQuery("#status").fadeOut();
+//    // will fade out the whole DIV that covers the website.
+//    jQuery("#preloader").delay(1000).fadeOut("slow");
+//})
+
+$(document).ready(function () {
+    $(window).load(function () {
+        $('#preloader-gif').fadeOut('slow', function () { $(this).remove(); });
+    });
+});
+
+
